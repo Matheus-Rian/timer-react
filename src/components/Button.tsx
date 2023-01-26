5f0e5c6024c3a34b8f69d2a078a238a8d7a2c7e1
Matheus Rian <souzamatheusrian@gmail.com>
2023-01-26 16:39:35 -0300

    refactor: remove code smell

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Button.styles'
-
-type Props = {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: Props) {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
