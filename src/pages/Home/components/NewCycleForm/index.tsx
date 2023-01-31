c01a85efc89117c3e54b326ea8bca6f5b5367565
Matheus Rian <souzamatheusrian@gmail.com>
2023-01-31 17:19:12 -0300

    feat: created component NewCycleForm

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,42 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { useFormContext } from 'react-hook-form'
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        type="text"
+        id="task"
+        placeholder="Dê um nome para o seu projeto"
+        list="tasks-suggestions"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="tasks-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="0"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
