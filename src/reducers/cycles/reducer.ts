8bc7addaa5dc41d2ce4e62446c396f1228971dea
Matheus Rian <souzamatheusrian@gmail.com>
2023-01-31 22:38:45 -0300

    refactor: using produce of immer instead hard code

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,4 +1,5 @@
 import { ActionTypes } from './actions'
+import { produce } from 'immer'
 
 export interface Cycle {
   id: string
@@ -17,31 +18,34 @@ interface CyclesState {
 export function cyclesReducer(actualState: CyclesState, action: any) {
   switch (action.type) {
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...actualState,
-        cycles: [...actualState.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      }
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...actualState,
-        cycles: actualState.cycles.map((cycle) => {
-          if (cycle.id === actualState.activeCycleId)
-            return { ...cycle, interruptedDate: new Date() }
-          else return cycle
-        }),
-        activeCycleId: null,
-      }
-    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-      return {
-        ...actualState,
-        cycles: actualState.cycles.map((cycle) => {
-          if (cycle.id === actualState.activeCycleId)
-            return { ...cycle, finishedDate: new Date() }
-          else return cycle
-        }),
-        activeCycleId: null,
-      }
+      return produce(actualState, (draft) => {
+        draft.cycles.push(action.payload.newCycle)
+        draft.activeCycleId = action.payload.newCycle.id
+      })
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = actualState.cycles.findIndex((cycle) => {
+        return cycle.id === actualState.activeCycleId
+      })
+
+      if (currentCycleIndex < 0) return actualState
+
+      return produce(actualState, (draft) => {
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].interruptedDate = new Date()
+      })
+    }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
+      const currentCycleIndex = actualState.cycles.findIndex((cycle) => {
+        return cycle.id === actualState.activeCycleId
+      })
+
+      if (currentCycleIndex < 0) return actualState
+
+      return produce(actualState, (draft) => {
+        draft.activeCycleId = null
+        draft.cycles[currentCycleIndex].finishedDate = new Date()
+      })
+    }
     default:
       return actualState
   }
